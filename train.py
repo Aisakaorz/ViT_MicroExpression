04446c7fae1da15cee8a5e767227ba4dfc92853c
AisakaManatsu <aisakaaoi@qq.com>
2022-10-02 18:24:38 +0800

    feat(train): 数据预处理 (1)添加CenterCrop、RandomHorizontalFlip预处理方法 (2)修改导入models库方法，先import再在实例模型时使用models.*进行初始化

--- train.py
+++ train.py
@@ -1,33 +1,37 @@
-import torch.optim as optim
 import torch.nn as nn
+import torch.optim as optim
 import torch.utils.data
 import torch.utils.data.distributed
+import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-import torchvision.datasets as datasets
-from models.vision_transformer import vit_base_patch16_224
 from torchtoolbox.tools import mixup_data, mixup_criterion
 from torchtoolbox.transform import Cutout
 from tqdm import tqdm
+
+import models
 
 # 设置全局参数
 LR = 1e-4
 BATCH_SIZE = 32
 EPOCHS = 100
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-alpha = 0.2
 ACC = 0
 
 if __name__ == '__main__':
     # 数据预处理
     transform_train = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.RandomHorizontalFlip(),
+        # transforms.RandomVerticalFlip(),
         Cutout(),
         transforms.ToTensor(),
         transforms.Normalize([0.50915915, 0.45180488, 0.4254344], [0.23399226, 0.22251633, 0.22062485])
     ])
     transform_test = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.50915915, 0.45180488, 0.4254344], [0.23399226, 0.22251633, 0.22062485])
     ])
@@ -38,32 +42,30 @@ if __name__ == '__main__':
 
     # 导入数据
     train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=BATCH_SIZE, shuffle=False)
 
     # 实例化模型并且移动到GPU
     criterion = nn.CrossEntropyLoss()
-    model_ft = vit_base_patch16_224(pretrained=True)
-    num_ftrs = model_ft.head.in_features
-    model_ft.head = nn.Linear(num_ftrs, 3, bias=True)
-    nn.init.xavier_uniform_(model_ft.head.weight)
-    model_ft.to(DEVICE)
+    model = models.vit_base_patch16_224(pretrained=True)
+    num_ftrs = model.head.in_features
+    model.head = nn.Linear(num_ftrs, 3, bias=True)
+    nn.init.xavier_uniform_(model.head.weight)
+    model.to(DEVICE)
 
     # 选择简单暴力的Adam优化器，学习率调低
-    adam = optim.Adam(model_ft.parameters(), lr=LR)
+    adam = optim.Adam(model.parameters(), lr=LR)
     optimizer = optim.lr_scheduler.CosineAnnealingLR(optimizer=adam, T_max=20, eta_min=1e-9)
 
     for epoch in range(1, EPOCHS + 1):
         # 训练
-        model_ft.train()
+        model.train()
         train_sum_loss = 0
-
-        # 进度条
         loop = tqdm(enumerate(train_loader), total=len(train_loader), leave=True)
         for batch_idx, (data, target) in loop:
             data, target = data.to(DEVICE, non_blocking=True), target.to(DEVICE, non_blocking=True)
-            data, labels_a, labels_b, lam = mixup_data(data, target, alpha)
+            data, labels_a, labels_b, lam = mixup_data(data, target)
             adam.zero_grad()
-            output = model_ft(data)
+            output = model(data)
             loss = mixup_criterion(criterion, output, labels_a, labels_b, lam)
             loss.backward()
             adam.step()
@@ -75,17 +77,16 @@ if __name__ == '__main__':
             loop.set_postfix(train_loss=loss.item(),
                              train_avg_loss=train_sum_loss / len(train_loader),
                              lr=lr)
-
         optimizer.step()
 
         # 验证
-        model_ft.eval()
+        model.eval()
         test_loss = 0
         correct = 0
         with torch.no_grad():
             for data, target in tqdm(test_loader):
                 data, target = data.to(DEVICE), target.to(DEVICE)
-                output = model_ft(data)
+                output = model(data)
                 loss = criterion(output, target)
                 _, pred = torch.max(output.data, 1)
                 correct += torch.sum(pred == target)
@@ -97,5 +98,5 @@ if __name__ == '__main__':
             print('Val set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
                 avgloss, correct, len(test_loader.dataset), 100 * acc))
             if acc > ACC:
-                torch.save(model_ft, 'model_' + str(epoch) + '_' + str(round(acc, 3)) + '.pth')
+                torch.save(model, 'model_' + str(epoch) + '_' + str(round(acc, 3)) + '.pth')
                 ACC = acc
